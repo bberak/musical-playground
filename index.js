787a6037db3a98de196649df90e65b319078dc17
bberak <bberak@gmail.com>
2019-12-05 01:53:41 +1100

    Added notes and octaves

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const Speaker = require("audio-speaker/stream");
 const Generator = require("audio-generator/stream");
 const readline = require("readline");
-const TWO_PI = Math.PI * 2;
+const two_pi = Math.PI * 2;
+const pitches = Object.freeze({
+  a: 0/12,
+  b: 2/12,
+  c: 3/12,
+  d: 5/12,
+  e: 7/12,
+  f: 8/12,
+  g: 10/12
+});
 
-let freq = 27.6;
+let octave = 4;
+let note = 'a';
 let amp = 1;
 
 Generator(time => {
+  const freq = 27.5 * Math.pow(2, octave + pitches[note]);
+
   return [
-    Math.sin(time * freq * TWO_PI) * amp,
-    Math.sin((time - 0.5) * freq * TWO_PI) * amp
+    Math.sin(time * two_pi * freq) * amp,
+    Math.sin(time * two_pi * freq) * amp,
   ];
 }).pipe(
   Speaker({
-    channels: 2
-    //sampleRate: 44100,
+    channels: 2,
+    //sampleRate: 27.5,
     //byteOrder: 'LE',
     //bitDepth: 16,
     //signed: true,
@@ -28,9 +40,16 @@ process.stdin.setRawMode(true);
 process.stdin.on("keypress", (str, key) => {
   if (key.ctrl && key.name === "c") process.exit();
 
-  if (key.name === "up") freq += 40;
+  if (key.name === "up") 
+    octave++;
+  
+  if (key.name === "down") 
+    octave--;
+  
+  if (pitches[key.name] !== undefined)
+    note = key.name;
 
-  if (key.name === "down") freq -= 40;
+  console.log(note, octave);
 });
 
 console.log("Press Control + C to exit");
