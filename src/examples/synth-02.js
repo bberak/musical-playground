59bfd49d177fe60340893788809f452c01a00789
bberak <bberak@gmail.com>
2019-12-14 00:27:10 +1100

    Added useful comments about previous attempts

--- src/examples/synth-02.js
+++ src/examples/synth-02.js
@@ -32,3 +32,26 @@ synthesizer(time => {
 }).play();
 
 exit();
+
+
+/*
+const master = scale(0.3)
+
+const track1 = signal(sine(440), master)(time);
+const track2 = signal(sine(880), scale(0.1), master)(time);
+const track3 = signal(sine(1320), scale(0.2), master)(time);
+const track4 = signal(sine(1760), scale(0.5), master)(time);
+
+return sum(track1, track2, track3, track4);
+*/
+
+/*
+return _.sum(
+	[
+		sine(440)(time),
+		sine(880)(time) * 0.1,
+		sine(1320)(time) * 0.2,
+		sine(1760)(time) * 0.5
+	].map(x => x * 0.3)
+);
+*/
