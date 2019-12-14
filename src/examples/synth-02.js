45a6dcaef4107ecdc4d0f032c3592e0b2d2c7a6a
bberak <bberak@gmail.com>
2019-12-14 13:28:26 +1100

    Another way to do it

--- src/examples/synth-02.js
+++ src/examples/synth-02.js
@@ -19,20 +19,28 @@ const { exit } = require("../utils");
 
 synthesizer(time => {
 
-	const master = gain(0.3);
+	const track1 = sine(440)(time);
+	const track2 = pipe(sine(880), scale(0.1))(time);
+	const track3 = pipe(sine(1320), scale(0.2))(time);
+	const track4 = pipe(sine(1760), scale(0.5))(time);
 
-	const track = master(
-		sine(440)(time),
-		chain(sine(880), scale(0.1))(time),
-		chain(sine(1320), scale(0.2))(time),
-		chain(sine(1760), scale(0.5))(time)
-	);
-
-	return track;
+	return  gain(0.3)(track1, track2, track3, track4);
 }).play();
 
 exit();
 
+/*
+const master = gain(0.3);
+
+const track = master(
+	sine(440)(time),
+	chain(sine(880), scale(0.1))(time),
+	chain(sine(1320), scale(0.2))(time),
+	chain(sine(1760), scale(0.5))(time)
+);
+
+return track;
+*/
 
 /*
 const master = scale(0.3)
